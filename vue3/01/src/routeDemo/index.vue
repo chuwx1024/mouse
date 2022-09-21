c2c8b724a10060c67d358c8326e911bd28b46ec9
chuhx <chuhx1024@126.com>
2022-09-21 22:46:13 +0800

    获取路由query传染

--- vue3/01/src/routeDemo/index.vue
+++ vue3/01/src/routeDemo/index.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
-    999999999999
+    姓名: {{ info.name }}<br>
+    年龄: {{ info.age }}
   </div>
 </template>
 
 <script setup>
+import { ref, reactive } from 'vue'
+import { useRouter, useRoute} from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+
+console.dir(route, '00000')
+console.log(route.query)
+
+const info = reactive({
+    name: route.query.name,
+    age: route.query.age
+})
+
+
 
 </script>
 
