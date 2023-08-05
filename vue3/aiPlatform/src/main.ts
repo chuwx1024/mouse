36c0b947965b82274165292ce20121966c6a28b2
chuhx <chuhx1024@126.com>
2023-08-05 21:46:58 +0800

    项目整合

--- vue3/aiPlatform/src/main.ts
+++ vue3/aiPlatform/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
+import '@/style/normalize.less'
 import './style.css'
 import App from './App.vue'
+
 
 createApp(App).mount('#app')
--- vue3/aiPlatform/vite.config.ts
+++ vue3/aiPlatform/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// import path from 'path'
-
-// function resovle (dir) {
-//   return path.resovle(__dirname, './', dir)
-// }
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
   // 解析
   resolve: {
     alias: {
